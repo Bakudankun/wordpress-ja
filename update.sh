e22a29c059db50fee25916032d1588fc983df369
Bakudankun <bakudankun@gmail.com>
2015-12-20 16:49:10 +0900

    Remove check-SHA1 function
    Because ja.wordpress.org doesn't provide SHA1 hash.

--- update.sh
+++ update.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-current="$(curl -sSL 'http://api.wordpress.org/core/version-check/1.7/' | sed -r 's/^.*"current":"([^"]+)".*$/\1/')"
-
-sha1="$(curl -sSL "https://wordpress.org/wordpress-$current.tar.gz.sha1")"
+current="4.4"
 
 travisEnv=
 for variant in apache fpm; do
@@ -12,7 +10,6 @@ for variant in apache fpm; do
 
 		sed -ri '
 			s/^(ENV WORDPRESS_VERSION) .*/\1 '"$current"'/;
-			s/^(ENV WORDPRESS_SHA1) .*/\1 '"$sha1"'/;
 		' "$variant/Dockerfile"
 
 		cp docker-entrypoint.sh "$variant/docker-entrypoint.sh"
